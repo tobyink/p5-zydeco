d303fabcde49541616bedc78c85baaffa8034ab9
Toby Inkster <mail@tobyinkster.co.uk>
2020-01-16 11:51:09 +0000

    bump version; changelog

--- lib/MooX/Pression.pm
+++ lib/MooX/Pression.pm
@@ -10,7 +10,7 @@ use MooX::Press::Keywords ();
 package MooX::Pression;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002';
+our $VERSION   = '0.003';
 
 use Keyword::Declare;
 use B::Hooks::EndOfScope;
