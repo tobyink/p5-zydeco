a1d920d302366530b646126cec69d3efbd802e49
Toby Inkster <mail@tobyinkster.co.uk>
2020-02-16 02:24:42 +0000

    clean up JSON encoder too

--- examples/simple-json-encoder.pl
+++ examples/simple-json-encoder.pl
@@ -1,6 +1,43 @@
-use v5.18;
-use strict;
-use warnings;
+use Zydeco prefix => 'MyApp';
+
+class JSON::Encoder {
+	multi method stringify (Undef $value) {
+		'null';
+	}
+	
+	multi method stringify (ScalarRef[Bool] $value) {
+		$$value ? 'true' : 'false';
+	}
+	
+	multi method stringify (Num $value) {
+		$value;
+	}
+	
+	multi method stringify :alias(quote_str) (Str $value)  {
+		sprintf(q<"%s">, quotemeta $value);
+	}
+	
+	multi method stringify (ArrayRef $arr) {
+		sprintf(
+			q<[%s]>,
+			join(q<,>, map($self->stringify($_), @$arr))
+		);
+	}
+	
+	multi method stringify (HashRef $hash) {
+		sprintf(
+			q<{%s}>,
+			join(
+				q<,>,
+				map sprintf(
+					q<%s:%s>,
+					$self->quote_str($_),
+					$self->stringify($hash->{$_}),
+				), sort keys %$hash
+			),
+		);
+	}
+}
 
 my $json = MyApp->new_json_encoder;
 
@@ -10,48 +47,3 @@ say $json->stringify({
 	baz  => \1,
 	quux => { xyzzy => 666 },
 });
-
-package MyApp {
-	use Zydeco;
-	
-	# A JSON encoder.
-	class JSON::Encoder {
-		multi method stringify (Undef $value) {
-			'null';
-		}
-		
-		multi method stringify (ScalarRef[Bool] $value) {
-			$$value ? 'true' : 'false';
-		}
-		
-		multi method stringify (Num $value) {
-			$value;
-		}
-		
-		# does not strictly follow JSON specs
-		multi method stringify :alias(quote_str) (Str $value)  {
-			sprintf(q<"%s">, quotemeta $value);
-		}
-		
-		multi method stringify (ArrayRef $arr) {
-			sprintf(
-				q<[%s]>,
-				join(q<,>, map($self->stringify($_), @$arr))
-			);
-		}
-		
-		multi method stringify (HashRef $hash) {
-			sprintf(
-				q<{%s}>,
-				join(
-					q<,>,
-					map sprintf(
-						q<%s:%s>,
-						$self->quote_str($_),
-						$self->stringify($hash->{$_}),
-					), sort keys %$hash
-				),
-			);
-		}
-	}
-}
