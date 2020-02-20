edf78dd43e7e9d7068a4c93f80e2a054589a35fe
Toby Inkster <mail@tobyinkster.co.uk>
2020-02-20 15:08:21 +0000

    remove pod from MooX::Pression to make it an even smaller stub

--- lib/MooX/Pression.pm
+++ lib/MooX/Pression.pm
@@ -1,51 +1,16 @@
+# MooX::Pression was renamed Zydeco.
+# This file is provided for backwards compatibility.
+#
+# See <http://zydeco.toby.ink/>.
+#
+
 use 5.014;
 use strict;
 use warnings;
-
 package MooX::Pression;
-
 use Zydeco ();
-
 our $AUTHORITY = 'cpan:TOBYINK';
 our $VERSION   = '0.508';
 our @ISA       = 'Zydeco';
-
-*PACKAGE_SPEC =\&Zydeco::PACKAGE_SPEC;
-
+*PACKAGE_SPEC  = \&Zydeco::PACKAGE_SPEC;
 1;
-
-__END__
-
-=pod
-
-=encoding utf-8
-
-=head1 NAME
-
-MooX::Pression - old name of Zydeco
-
-=head1 DESCRIPTION
-
-This package exists for backwards compatibility.
-
-=head1 SEE ALSO
-
-L<Zydeco>.
-
-=head1 AUTHOR
-
-Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
-
-=head1 COPYRIGHT AND LICENCE
-
-This software is copyright (c) 2020 by Toby Inkster.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=head1 DISCLAIMER OF WARRANTIES
-
-THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
-MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-
