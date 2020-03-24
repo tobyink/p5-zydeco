93b705a705297d259254e0fe542b3329063297c2
Toby Inkster <mail@tobyinkster.co.uk>
2020-03-24 18:19:29 +0000

    bump version; changelog

--- lib/MooX/Pression.pm
+++ lib/MooX/Pression.pm
@@ -10,7 +10,7 @@ use warnings;
 package MooX::Pression;
 use Zydeco ();
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.514';
+our $VERSION   = '0.515';
 our @ISA       = 'Zydeco';
 *PACKAGE_SPEC  = \&Zydeco::PACKAGE_SPEC;
 1;
