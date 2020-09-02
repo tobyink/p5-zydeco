812ee293de590588d1c5871c423d1fe93221632e
Toby Inkster <mail@tobyinkster.co.uk>
2020-09-02 19:39:24 +0100

    bump version; changelog

--- lib/MooX/Pression.pm
+++ lib/MooX/Pression.pm
@@ -10,7 +10,7 @@ use warnings;
 package MooX::Pression;
 use Zydeco ();
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.519';
+our $VERSION   = '0.520';
 our @ISA       = 'Zydeco';
 *PACKAGE_SPEC  = \&Zydeco::PACKAGE_SPEC;
 1;
--- lib/Zydeco.pm
+++ lib/Zydeco.pm
@@ -12,7 +12,7 @@ use feature ();
 package Zydeco;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.519';
+our $VERSION   = '0.520';
 
 use Keyword::Simple ();
 use PPR;
@@ -2322,7 +2322,7 @@ sub _include {
 #{
 #	package Zydeco::Anonymous::Package;
 #	our $AUTHORITY = 'cpan:TOBYINK';
-#	our $VERSION   = '0.519';
+#	our $VERSION   = '0.520';
 #	use overload q[""] => sub { ${$_[0]} }, fallback => 1;
 #	sub DESTROY {}
 #	sub AUTOLOAD {
@@ -2333,7 +2333,7 @@ sub _include {
 #	
 #	package Zydeco::Anonymous::Class;
 #	our $AUTHORITY = 'cpan:TOBYINK';
-#	our $VERSION   = '0.519';
+#	our $VERSION   = '0.520';
 #	our @ISA       = qw(Zydeco::Anonymous::Package);
 #	sub new {
 #		my $me = shift;
@@ -2346,12 +2346,12 @@ sub _include {
 #	
 #	package Zydeco::Anonymous::Role;
 #	our $AUTHORITY = 'cpan:TOBYINK';
-#	our $VERSION   = '0.519';
+#	our $VERSION   = '0.520';
 #	our @ISA       = qw(Zydeco::Anonymous::Package);
 #	
 #	package Zydeco::Anonymous::ParameterizableClass;
 #	our $AUTHORITY = 'cpan:TOBYINK';
-#	our $VERSION   = '0.519';
+#	our $VERSION   = '0.520';
 #	our @ISA       = qw(Zydeco::Anonymous::Package);
 #	sub generate_package {
 #		my $me  = shift;
@@ -2365,7 +2365,7 @@ sub _include {
 #
 #	package Zydeco::Anonymous::ParameterizableRole;
 #	our $AUTHORITY = 'cpan:TOBYINK';
-#	our $VERSION   = '0.519';
+#	our $VERSION   = '0.520';
 #	our @ISA       = qw(Zydeco::Anonymous::Package);
 #	sub generate_package {
 #		my $me  = shift;
